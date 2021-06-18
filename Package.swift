bc51d603e52e6b70c9cceacfa4e5853054999a9f
sutefu23 <sutefu23@gmail.com>
2021-06-18 13:46:11 +0900

    ResourcesフォルダをPackageに追加

--- Package.swift
+++ Package.swift
@@ -20,7 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DMSwift",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Resources"),
+            ]
+        ),
         .testTarget(
             name: "DMSwiftTests",
             dependencies: ["DMSwift"]),
